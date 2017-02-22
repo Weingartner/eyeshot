86302b9d4ac63856d2f6a9abf5283c3d184c2d38
bradphelan <b.phelan@weingartner.com>
2017-02-22 16:18:21 +0100

    add push script

---
+++ push.ps1
@@ -0,0 +1,12 @@
+$dir = "C:\test_code"
+$file = dir *.nupkg |
+  Sort-Object { 
+        [Version] $(if ($_.BaseName -match "(\d+.){3}\d+") { 
+                        $matches[0]
+                    } 
+                    else { 
+                        "0.0.0.0"
+                    })  
+    } | select -last 1 -ExpandProperty Name
+
+nuget push -Verbosity detailed -source nuget.org $file
